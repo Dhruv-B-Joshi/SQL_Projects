ad20be02cd3610f1fbc2db30ebc5b56fed8e7bf9
Dhruv-B-Joshi <officiallytodhruv@gmail.com>
2024-09-29 14:02:17 +0530

    Update Simple_queries.sql

--- Simple_queries.sql
+++ Simple_queries.sql
@@ -1,4 +1,4 @@
--- Creating the structure of the table
+-- Creating the structure of the table.
 CREATE TABLE movies (
 	film_name VARCHAR(50),
 	genre VARCHAR(15),
@@ -9,12 +9,12 @@ CREATE TABLE movies (
 	gross FLOAT,
 	release_year SMALLINT);
 	
--- Imported data from local csv file to our table and overviewing it (we can also use COPY table_name FROM)
+-- Importing data from local csv file to our table and overviewing it. (we can also use COPY table_name FROM)
 SELECT * FROM movies
+LIMIT 5;
 
-/* We can observe some fields containing Null values, we will delete these rows.*/ 
+-- We can observe some fields containing Null values, we will delete these rows.
 DELETE FROM movies WHERE (movies IS NULL);
-
 
 /* 
 Q1 : Genre which is most loved by the audience? 
@@ -29,17 +29,8 @@ FROM movies
 GROUP BY genre
 ORDER BY audience_avg DESC;
 
-/* 
-Q2 : Top 10 grossing movies ? 
-Ordering data in descending based on gross profit of the movies and limiting by 10
-*/
-SELECT film_name, studio, gross, profit
-FROM movies
-ORDER BY gross DESC
-LIMIT 10;
-
 /*
-Q3 : Find the most profitable studios across the years?
+Q2 : Finding the most profitable studios across the years?
 Calculating and ordering the sum of profit and grouping the data by studios while
 removing inappropriate data (Independent studio films)
 */
@@ -49,20 +40,14 @@ WHERE (studio!='Independent')
 GROUP BY studio
 ORDER BY sum_of_profit DESC;
 
-/* Similarly viewing most profitable independent movies */ 
+-- Similarly viewing most profitable independent movies
 SELECT film_name, profit FROM movies
 WHERE studio ='Independent'
-ORDER BY profit DESC;
+ORDER BY profit DESC
+LIMIT 5;
 
-/* Profit of studios by year */
-SELECT studio, release_year, profit :: NUMERIC(10,2) ,
-SUM(profit) OVER(partition BY studio) AS sum_of_profit
-FROM movies
-WHERE studio!= 'Independent'
-ORDER BY sum_of_profit DESC;
-
-/* Year wise profit of all the studios (except independent) ?
-   Here we will use crosstab to get the desired result*/
+/* Q3 : Year wise profit of all the studios (except independent) ?
+   Here we will use crosstab to get the desired result. */
 CREATE extension tablefunc;
 SELECT * FROM crosstab 
 	('SELECT studio, release_year, SUM(profit) :: NUMERIC(10,2) as sp 
@@ -70,14 +55,5 @@ SELECT * FROM crosstab
 	 WHERE studio != ''Independent''
 	 GROUP BY studio, release_year
 	 ORDER BY 1') 
-AS Yearwiseprofit(Studio VARCHAR(25), Year_2007 NUMERIC(10,2),Year_2008 NUMERIC(10,2),Year_2009 NUMERIC(10,2),Year_2010 NUMERIC(10,2),Year_2011 NUMERIC(10,2));
-
-
-
-
-
-
-
-
-
-
+AS Yearwiseprofit(Studio VARCHAR(25), Year_2007 NUMERIC(10,2),Year_2008 NUMERIC(10,2),
+				  Year_2009 NUMERIC(10,2),Year_2010 NUMERIC(10,2),Year_2011 NUMERIC(10,2));
